9df30104339be03b6d2f3b8fa10c589b203889f1
doyoon0817 <leedoyoon0817@gmail.com>
2025-11-20 11:10:39 +0900

    Update Lepton_I2C.h

--- lepton/raspberrypi_video/Lepton_I2C.h
+++ lepton/raspberrypi_video/Lepton_I2C.h
@@ -1,7 +1,8 @@
-#ifndef LEPTON_I2C
-#define LEPTON_I2C
+//중복 포함 방지
+#ifndef LEPTON_I2C//"LEPTON_I2C"가 아직 정의되지 않았다면
+#define LEPTON_I2C//"LEPTON_I2C"를 정의
 
-void lepton_perform_ffc();
-void lepton_reboot();
+void lepton_perform_ffc(); // FFC(보정) 수행
+void lepton_reboot();//Lepton 카메라 모듈 재부팅
 
-#endif
+#endif // 중복 포함 방지 끝
